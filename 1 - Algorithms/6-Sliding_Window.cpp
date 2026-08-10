afb075f9c0bff6d4e529a08de9ac8a6b099ff912
Nikhil Guleria <163378911+NikhilGuleria2004@users.noreply.github.com>
2026-08-10 16:41:29 +0530

    Create 6-Sliding_Window.cpp

---
+++ 1 - Algorithms/6-Sliding_Window.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int maxSum(vector<int>&arr, int k){
+    int windowsSum = 0;
+    int maxSum = 0;
+    
+    for(int i=0;i<k;i++){
+        windowsSum+=arr[i];
+    }
+    
+    maxSum = windowsSum;
+    for(int i=k; i<arr.size(); i++){
+        windowsSum+=arr[i];
+        windowsSum-=arr[i-k];
+        
+        maxSum = max(maxSum, windowsSum);
+    }
+    
+    return maxSum;
+}
+
+int main(){
+    vector<int>arr = {2,1,5,1,3,2};
+    cout<<maxSum(arr,3);
+}
